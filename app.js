577bff77114e25f5e6b4a11271d677d776316155
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-06-15 17:40:18 +0500

    route

--- app.js
+++ app.js
@@ -1,6 +1,101 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const { get } = require('http');
 
-app.get('/home', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+const tours = JSON.parse(
+  fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
+);
+
+app.use(express.json());
+
+const getTour = (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    data: 'siz datani oldingiz',
+  });
+};
+
+const postTour = (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    data: 'siz datani oldingiz',
+  });
+};
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    data: tours,
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const data = req.body;
+  const newId = tours[tours.length - 1].id + 1;
+  data.id = newId;
+  tours.push(data);
+  fs.writeFileSync(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => console.log(err)
+  );
+  res.status(201).json({
+    data: tours,
+  });
+  console.log(data);
+});
+
+////////////////////
+
+const getTourById = (req, res) => {
+  const id = req.params.id;
+  const data = tours.find((val) => {
+    return val.id == id;
+  });
+  console.log(data);
+  if (data) {
+    res.status(200).json({
+      status: 'Succces',
+      data: data,
+    });
+  }
+};
+
+const updateTour = (req, res) => {
+  const id = req.params.id;
+  if (id > tours.length) {
+    return res.status(404).json({
+      message: 'Bunday id topilmadi',
+    });
+  }
+  res.status(200).json({
+    status: 'Succes',
+    message: 'The data has updated',
+  });
+};
+const deleteTour = (req, res) => {
+  const id = req.params.id;
+  if (id > tours.length) {
+    return res.status(404).json({
+      message: 'Bunday id topilmadi',
+    });
+  }
+  res.status(200).json({
+    status: 'Succes',
+    message: 'The data has deleted',
+  });
+};
+
+app.route('/api/v1/tours').get(getTour).post(postTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTourById)
+  .post(updateTour)
+  .delete(deleteTour);
+
+// /////////////////
+
+app.listen(8000, () => {
+  console.log('server is running');
 });
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const fs = require('fs');
-const http = require('http');
+// const fs = require('fs');
+// const http = require('http');
 
-const data = fs.readFileSync(`./data.json`, 'utf-8');
-// console.log(data);
+// const data = fs.readFileSync(`./data.json`, 'utf-8');
+// // console.log(data);
 
-const server = http.createServer((req, res) => {
-  const urlcha = req.url;
-  if (urlcha === '/api') {
-    res.end(data);
-  } else {
-    res.writeHead(404, {
-      'Content-Type': 'text/html',
-      'my-own-header': 'fail',
-      q,
-    });
-    res.end('<h1>404</h1>');
-  }
-});
+// const server = http.createServer((req, res) => {
+//   const urlcha = req.url;
+//   if (urlcha === '/api') {
+//     res.end(data);
+//   } else {
+//     res.writeHead(404, {
+//       'Content-Type': 'text/html',
+//       'my-own-header': 'fail',
+//     });
+//     res.end('<h1>zaybal</h1>');
+//   }
+// });
 
-server.listen(7001, '127.0.0.1', () => {
-  console.log('server is running');
-});
+// server.listen(8000, '127.0.0.1', () => {
+//   console.log('server is running');
+// });
